49fef474b89da164068bf8728522d7b47a45b944
bianyishupian <bianyichips@outlook.com>
2023-07-19 20:58:51 +0800

    channel

---
+++ net/channel.cc
@@ -0,0 +1,96 @@
+#include <sys/epoll.h>
+
+#include "channel.h"
+#include "eventloop.h"
+#include "mymuduo11/base/logger.h"
+
+const int Channel::NoneEvent = 0;
+const int Channel::ReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::WriteEvent = EPOLLOUT;
+
+Channel::Channel(EventLoop *loop, int fd)
+    : _loop(loop), _fd(fd), _events(0), _revents(0), _index(-1), _tied(false) {}
+
+Channel::~Channel() {}
+
+// channel的tie方法什么时候调用过?  TcpConnection => channel
+/**
+ * TcpConnection中注册了Chnanel对应的回调函数，传入的回调函数均为TcpConnection
+ * 对象的成员方法，因此可以说明一点就是：Channel的结束一定早于TcpConnection对象！
+ * 此处用tie去解决TcoConnection和Channel的生命周期时长问题，从而保证了Channel对
+ * 象能够在TcpConnection销毁前销毁。
+ **/
+void Channel::tie(const std::shared_ptr<void> &obj)
+{
+    _tie = obj;
+    _tied = true;
+}
+
+/**
+ * 当改变channel所表示的fd的events事件后，update负责再poller里面更改fd相应的事件epoll_ctl
+ **/
+void Channel::update()
+{
+    // 通过channel所属的eventloop，调用poller的相应方法，注册fd的events事件
+    _loop->updateChannel(this);
+}
+
+// 在channel所属的EventLoop中把当前的channel删除掉
+void Channel::remove()
+{
+    _loop->removeChannel(this);
+}
+
+void Channel::handleEvent(Timestamp receiveTime)
+{
+    if (_tied)
+    {
+        std::shared_ptr<void> guard = _tie.lock();
+        if (guard)
+        {
+            handleEventWithGuard(receiveTime);
+        }
+        // 如果提升失败了 就不做任何处理 说明Channel的TcpConnection对象已经不存在了
+    }
+    else
+    {
+        handleEventWithGuard(receiveTime);
+    }
+}
+
+void Channel::handleEventWithGuard(Timestamp receiveTime)
+{
+    LOG_INFO("channel handleEvent revents:%d\n", _revents);
+    // 关闭
+    if ((_revents & EPOLLHUP) && !(_revents & EPOLLIN)) // 当TcpConnection对应Channel 通过shutdown 关闭写端 epoll触发EPOLLHUP
+    {
+        if (_closeCallback)
+        {
+            _closeCallback();
+        }
+    }
+    // 错误
+    if (_revents & EPOLLERR)
+    {
+        if (_errorCallback)
+        {
+            _errorCallback();
+        }
+    }
+    // 读
+    if (_revents & (EPOLLIN | EPOLLPRI))
+    {
+        if (_readCallback)
+        {
+            _readCallback(receiveTime);
+        }
+    }
+    // 写
+    if (_revents & EPOLLOUT)
+    {
+        if (_writeCallback)
+        {
+            _writeCallback();
+        }
+    }
+}
---
+++ net/channel.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+
+#include "mymuduo11/base/noncopyable.h"
+#include "mymuduo11/base/timestamp.h"
+
+class EventLoop;
+
+class Channel : noncopyable
+{
+public:
+    using EventCallback = std::function<void()>;
+    using ReadEventCallback = std::function<void(Timestamp)>;
+
+    Channel(EventLoop *loop, int fd);
+    ~Channel();
+
+    // fd得到Poller通知以后 处理事件 handleEvent在EventLoop::loop()中调用
+    void handleEvent(Timestamp receiveTime);
+
+    // 设置回调函数对象
+    void setReadCallback(ReadEventCallback cb) { _readCallback = std::move(cb); }
+    void setWriteCallback(EventCallback cb) { _writeCallback = std::move(cb); }
+    void setCloseCallback(EventCallback cb) { _closeCallback = std::move(cb); }
+    void setErrorCallback(EventCallback cb) { _errorCallback = std::move(cb); }
+
+    // 防止当channel被手动remove掉 channel还在执行回调操作
+    void tie(const std::shared_ptr<void> &);
+
+    int fd() const { return _fd; }
+    int events() const { return _events; }
+    void set_revents(int revt) { _revents = revt; }
+
+    // 设置fd相应的事件状态 相当于epoll_ctl add delete
+    void enableReading() { _events |= ReadEvent; update(); }
+    void disableReading() { _events &= ~ReadEvent; update(); }
+    void enableWriting() { _events |= WriteEvent; update(); }
+    void disableWriting() { _events &= ~WriteEvent; update(); }
+    void disableAll() { _events = NoneEvent; update(); }
+
+    // 返回fd当前的事件状态
+    bool isNoneEvent() const { return _events == NoneEvent; }
+    bool isWriting() const { return _events & WriteEvent; }
+    bool isReading() const { return _events & ReadEvent; }
+
+    int index() { return _index; }
+    void set_index(int idx) { _index = idx; }
+
+    // one loop per thread
+    EventLoop *ownerLoop() { return _loop; }
+    void remove();
+
+private:
+
+    void update();
+    void handleEventWithGuard(Timestamp receiveTime);
+
+    static const int NoneEvent;
+    static const int ReadEvent;
+    static const int WriteEvent;
+
+    EventLoop *_loop;
+    const int _fd;
+    int _events;
+    int _revents;       // poller返回的事件
+    int _index;
+
+    std::weak_ptr<void> _tie;   // 延长生命周期
+    bool _tied;
+
+    ReadEventCallback _readCallback;
+    EventCallback _writeCallback;
+    EventCallback _closeCallback;
+    EventCallback _errorCallback;
+};
