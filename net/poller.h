8cb1045c806a532d1b1a734ff582e3e88d1a4b61
bianyishupian <bianyichips@outlook.com>
2023-07-19 20:32:07 +0800

    poller

---
+++ net/poller.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+
+#include "mymuduo11/base/noncopyable.h"
+#include "mymuduo11/base/timestamp.h"
+
+class Channel;
+class EventLoop;
+
+// Base class for IO Multiplexing
+
+class Poller : noncopyable
+{
+public:
+    using ChannelList = std::vector<Channel*>;
+    Poller(EventLoop *loop);
+    virtual ~Poller() = default;
+
+    // 给所有IO复用保留统一的接口
+    virtual Timestamp poll(int timeoutMs, ChannelList *activeChannels) = 0;
+    virtual void updateChannel(Channel *channel) = 0;
+    virtual void removeChannel(Channel *channel) = 0;
+
+    // 判断参数channel是否在当前的Poller当中
+    bool hasChannel(Channel *channel) const;
+    // EventLoop可以通过该接口获取默认的IO复用的具体实现
+    static Poller *newDefaultPoller(EventLoop *loop);
+protected:
+    using ChannelMap = std::unordered_map<int, Channel*>;
+    ChannelMap _channels;
+private:
+    EventLoop *_ownerLoop;
+};
+
