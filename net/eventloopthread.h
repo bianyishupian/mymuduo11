5d9fc28b34cf6170c08b6be3bd40996ed848a93c
bianyishupian <bianyichips@outlook.com>
2023-07-22 19:16:59 +0800

    EventLoopThread

---
+++ net/eventloopthread.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+
+#include "../base/noncopyable.h"
+#include "../base/Thread.h"
+
+class EventLoop;
+
+class EventLoopThread : noncopyable
+{
+public:
+    using ThreadInitCallback = std::function<void(EventLoop *)>;
+    EventLoopThread(const ThreadInitCallback &cb = ThreadInitCallback(),
+                    const std::string &name = std::string());
+    ~EventLoopThread();
+    EventLoop *startLoop();
+
+private:
+    void threadFunc();
+
+    EventLoop *_loop;
+    bool _exiting;
+    Thread _thread;
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    ThreadInitCallback _callback;
+};
